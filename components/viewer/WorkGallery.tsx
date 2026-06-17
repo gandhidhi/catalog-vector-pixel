089aaf2a9603f9e506a9d7e6ec98e3bdb1948bd2
gandhidhi <yu-suzuki@kua.kyoto-art.ac.jp>
2026-06-17 18:51:14 +0900

    feat: show 5 cards per row on xl screens

--- components/viewer/WorkGallery.tsx
+++ components/viewer/WorkGallery.tsx
@@ -48,7 +48,7 @@ export default function WorkGallery({
   return (
     <div>
       {/* Grid */}
-      <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 items-start">
+      <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 items-start">
         {works.map((work) => (
           <WorkCard
             key={work.id}
